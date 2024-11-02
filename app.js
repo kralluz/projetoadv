afffab0a042f14b895a8f5103c12316dcfe8a02f
kralluz <chenrique201520@gmail.com>
2024-11-02 17:38:21 -0300

    a

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Rota principal (opcional)
 app.get("/", (req, res) => {
+  console.log("Servidor Backend rodando.");
   res.send("Servidor Backend rodando.");
 });
 
@@ -70,87 +71,6 @@ const isAuthenticated = (req, res, next) => {
     res.status(401).send("Usuário não autenticado.");
   }
 };
-
-app.get("/api/contacts", isAuthenticated, async (req, res) => {
-  const accessToken = req.session.access_token;
-
-  if (!accessToken) {
-    return res.status(400).send("Access token não fornecido.");
-  }
-
-  try {
-    const contactsResponse = await axios.get(
-      "https://www.zohoapis.com/bigin/v2/users?type=AllUsers",
-      {
-        headers: {
-          Authorization: `Zoho-oauthtoken ${accessToken}`,
-        },
-      }
-    );
-
-    res.json(contactsResponse.data);
-  } catch (error) {
-    console.error("Erro ao obter contatos:", error.response?.data);
-
-    // Se o token expirou, tentar renovar usando o refresh token
-    if (error.response && error.response.status === 401) {
-      const refreshToken = req.session.refresh_token;
-
-      if (!accessToken) {
-        return res.status(400).send("Access token não fornecido.");
-      }
-      if (refreshToken) {
-        try {
-          const tokenResponse = await axios.post(
-            "https://accounts.zoho.com/oauth/v2/token",
-            new URLSearchParams({
-              client_id: process.env.CLIENT_ID,
-              client_secret: process.env.CLIENT_SECRET,
-              grant_type: "refresh_token",
-              refresh_token: refreshToken,
-            }).toString(),
-            {
-              headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-              },
-            }
-          );
-
-          const { access_token: newAccessToken } = tokenResponse.data;
-          req.session.access_token = newAccessToken;
-
-          // Tentar novamente obter os contatos com o novo access token
-          const newContactsResponse = await axios.get(
-            "https://www.zohoapis.com/bigin/v2/users?type=AllUsers",
-            {
-              headers: {
-                Authorization: `Zoho-oauthtoken ${newAccessToken}`,
-              },
-            }
-          );
-
-          res.json(newContactsResponse.data);
-        } catch (refreshError) {
-          console.error(
-            "Erro ao renovar o access token:",
-            refreshError.response?.data
-          );
-          res
-            .status(500)
-            .send(
-              "Falha ao renovar a autenticação. Por favor, faça login novamente."
-            );
-        }
-      } else {
-        res
-          .status(401)
-          .send("Refresh token não disponível. Faça login novamente.");
-      }
-    } else {
-      res.status(500).send("Falha ao obter contatos.");
-    }
-  }
-});
 
 // Rota para exibir a página de contatos (opcional)
 app.get("/dashboard", (req, res) => {
